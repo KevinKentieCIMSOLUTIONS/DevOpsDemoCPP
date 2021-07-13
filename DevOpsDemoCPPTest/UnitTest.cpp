a9b4cf4a6fd6c5f9600e8aa8d8a815690fc66fd4
KevinKentieCIMSOLUTIONS <k.kentie@cimsolutions.nl>
2021-07-13 14:54:22 +0200

    Add project files.

---
+++ DevOpsDemoCPPTest/UnitTest.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include <iostream>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace DevOpsDemoCPPTest
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+        TEST_METHOD(TestMethod1)
+        {
+            int x = 1;
+        }
+    };
+}
