454305bad805f7be143c113041802ebc96037500
KevinKentieCIMSOLUTIONS <k.kentie@cimsolutions.nl>
2021-07-20 15:08:36 +0200

    new test format

---
+++ CPPDemo_UnitTest/CPPDemo_UnitTest.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CPPDemoUnitTest
+{
+	TEST_CLASS(CPPDemoUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+				int x = 1;
+		}
+	};
+}
